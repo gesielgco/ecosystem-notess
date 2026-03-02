b73a8b33966afe2ba8d8cfe0685614db831a892d
gesielgco <gesielgco@gmail.com>
2026-03-02 15:24:48 +0000

    chore: fix tailwind setup and move configs to app folder

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,9 +1,7 @@
 export default function App() {
   return (
     <div className="min-h-screen bg-red-500 flex items-center justify-center">
-      <h1 className="text-5xl font-bold text-white">
-        TESTE VISUAL
-      </h1>
+      <h1 className="text-5xl font-bold text-white">TESTE VISUAL</h1>
     </div>
   );
 }
