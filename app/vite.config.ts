e743be11354e498a70d428c1dbd3a77de6725f23
gesielgco <gesielgco@gmail.com>
2026-04-26 19:20:47 +0000

    fix: corrige tela branca e icones apple

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "./", // 👈 ESSA LINHA É O QUE FALTA
+  base: "/", // 👈 ESSA LINHA É O QUE FALTA
 })
